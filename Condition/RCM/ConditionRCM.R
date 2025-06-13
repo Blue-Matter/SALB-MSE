43475cf118824980c2f6841544071290470dcd0a
Adrian Hordyk <ar.hordyk@gmail.com>
2025-06-13 09:06:48 -0700

    updates

---
+++ Condition/RCM/ConditionRCM.R
@@ -0,0 +1,184 @@
+
+library(openMSE)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+# Demo: following the 2020 assessment (ASPIC SCRS/2020/095 & JABBA SCRS/2020/104) using data up to 2018 
+
+
+# Data 
+
+# Fleets:
+# Catch: 
+# 1. Chinese Taipei & Korea (LL)
+# 2. China, E.C Spain, E.C Portugal, Japan, Philippines, St Vincent & Grenadier, USA, Vanuatu, Honduras, Nei, Cote D'Ivoire, E.U UK, Seychelles, U.K Sta Helena, Angola, Senegal, Trinidad & Tobago (1956 - 1969)
+# 3. As 2 (1970 - 1975)
+# 4. As 2 (1976 - 2018)
+# 5. Brazil, Panama, South Africa, Argentina, Belize, Cambodia, Cuba, Namibia
+# 6. 1956 - 1998 Brazil etc
+# 7. 1999 - 2018 Brazil etc
+# 8. Uruguay
+#
+# CPUE: (1956 - 2018) 
+# Three CPUE series used in 2020 assessment
+# 1. Chinese Taipei (LL)
+# 2. -
+# 3. -
+# 4. Japan (LL) 1976 - 2018
+# 5. -
+# 6. -
+# 7. -
+# 8. Uruguay (LL)
+
+
+readxl::excel_sheets('Data/SALB_2018.xlsx')
+
+RCMData <- new('RCMdata') 
+
+# ----- Catch Data -----
+CatchData <- readxl::read_excel('Data/SALB_2018.xlsx', "Catch")
+
+# Filtering data up to 2018 for demo - this can be removed later
+CatchData <- CatchData |> dplyr::filter(Year<=2018)  
+
+CatchTotal <- CatchData |> dplyr::group_by(Year) |> dplyr::summarise(n=sum(Catch))
+
+Year1 <- CatchTotal |> dplyr::filter(n>0) |> dplyr::summarise(Year1=min(Year))
+
+CatchData <- CatchData |> dplyr::filter(Year>=Year1$Year1)
+
+Fleets <- CatchData$Fleet |> unique()
+Years <- CatchData$Year |> unique() |> sort()
+nFleet <- length(Fleets)
+nYears <- length(Years)
+
+RCMData@Chist <- array(1E-8, c(nYears, nFleet), dimnames = list(Year=Years, Fleet=Fleets))
+
+for (fl in Fleets) {
+  catchdata <- CatchData |> dplyr::filter(Fleet==fl) 
+  abind::afill(RCMData@Chist) <- array(catchdata$Catch, dim=c(length(catchdata$Catch), 1), 
+                                       dimnames = list(Year=catchdata$Year, Fleet=unique(catchdata$Fleet))
+  )
+}
+
+# ---- CPUE Data ----
+CPUEData <- readxl::read_excel('Data/SALB_2018.xlsx', "CPUE")
+
+# Filtering data up to 2018 for demo - this can be removed later
+CPUEData <- CPUEData |> dplyr::filter(Year<=2018)
+
+CPUEFleets <- CPUEData$Fleet |> unique()
+
+RCMData@Index <- array(NA, c(nYears, nFleet), dimnames = list(Year=Years, Fleet=Fleets))
+RCMData@I_sd <- RCMData@Index
+for (fl in CPUEFleets) {
+  cpuedata <- CPUEData |> dplyr::filter(Fleet==fl) 
+  cpuedata$CPUE <- cpuedata$CPUE/mean(cpuedata$CPUE, na.rm=TRUE)
+  abind::afill(RCMData@Index) <- array(cpuedata$CPUE, dim=c(length(cpuedata$CPUE), 1), 
+                                       dimnames = list(Year=cpuedata$Year, Fleet=unique(cpuedata$Fleet))
+  )
+  abind::afill(RCMData@I_sd) <- array(cpuedata$CV, dim=c(length(cpuedata$CPUE), 1), 
+                                       dimnames = list(Year=cpuedata$Year, Fleet=unique(cpuedata$Fleet))
+  )
+}
+
+# ----- CAL Data ----
+CALData <- readxl::read_excel('Data/SALB_2018.xlsx', "CAL")
+
+# Filtering data up to 2018 for demo - this can be removed later
+CALData <- CALData |> dplyr::filter(Year<=2018)
+
+CALFleets <- CALData$Fleet |> unique()
+
+
+length_bin <- as.numeric(colnames(CALData))
+length_bin <- length_bin[!is.na(length_bin)]
+nBin <- length(length_bin)
+
+RCMData@CAL <- array(NA, c(nYears, nBin, nFleet), dimnames = list(Year=Years,
+                                                                  Class=length_bin,
+                                                                  Fleet=Fleets))
+CAL_ESS <- 100 
+for (fl in CALFleets) {
+  caldataFleet <- CALData |> dplyr::filter(Fleet==fl) 
+  caldata <- caldataFleet[,3:ncol(caldataFleet)] |> as.matrix()
+  abind::afill(RCMData@CAL) <- array(caldata, dim=c(nrow(caldata), ncol(caldata), 1), 
+                                       dimnames = list(Year=caldataFleet$Year, 
+                                                       Class=length_bin,
+                                                       Fleet=unique(caldataFleet$Fleet)))
+  
+}
+
+RCMData@CAL_ESS <- pmin(apply(RCMData@CAL, c(1, 3), sum, na.rm = TRUE), CAL_ESS)
+RCMData@CAL_ESS[RCMData@CAL_ESS==0] <- NA
+RCMData@length_bin <- length_bin
+
+# ---- Base Case OM Parameters -----
+
+OM_Base <- new("OM")
+OM_Base@nyears <- nYears
+OM_Base@nsim <- 2
+OM_Base@Species <- "Thunnus alalunga"
+OM_Base@maxage <- 20
+OM_Base@CurrentYr <- 2018
+
+# Natural Mortality, Linf, K, t0, 
+OM_Base <- LH2OM(OM_Base) # direct from FishBase via FishLife
+
+OM_Base@t0 <- c(-1.89, -0.98) # range from ICCAT Manual 2.1.4 ALB
+# Growth spans uncertainty in ICCAT Manual 2.1.4 ALB
+# L50 mean pretty close to that reported in SCRS/2024/156
+
+OM_Base@cpars$L95 <- OM_Base@cpars$L50 * 1.16 # ratio reported in SCRS/2024/156
+
+# Length-Weight relationship
+OM_Base@a <- 1.3718E-5
+OM_Base@b <- 3.09773 # Penney (1994) from ICCAT Manual 2.1.4 ALB
+
+# Steepness
+OM_Base@h <- c(0.75, 0.9) # uniform distribution borrowed from North Atlantic Albacore MSE https://www.iccat.int/Documents/CVSP/CV076_2019/n_8/CV07608051.pdf
+
+# Stock-Recruit Relationship
+OM_Base@SRrel <- 1 # Beverton Holt SRR
+
+# Process Error
+OM_Base@Perr <- c(0.2, 0.4) # uniform distribution - assumed based on nothing in particular
+
+# Selectivity Starting Values 
+CAL <- array2DF(RCMData@CAL)
+CAL$Year <- as.numeric(CAL$Year)
+CAL$Class <- as.numeric(CAL$Class)
+CAL$Fleet <- as.numeric(CAL$Fleet)
+
+CAL <- CAL |> dplyr::group_by(Class) |>
+  dplyr::summarise(n=sum(Value, na.rm=TRUE))
+
+
+ggplot(CAL, aes(x=Class, y=n)) +
+  geom_line() +
+  theme_bw()
+
+CAL$Class[which.max(CAL$n)]
+
+# initial starting values - estimated by fleet in RCM
+OM_Base@L5 <- c(50, 60)
+OM_Base@LFS <- c(80, 90)
+OM_Base@Vmaxlen <- c(1,1)
+
+
+OM_Base@R0 <- 10000 # initial value 
+
+# ---- Condition Base Case OM ----
+
+RCM_Base <- RCM(OM_Base, RCMData)
+
+plot(RCM_Base)
+
+
+# Alternative Assumptions:
+# 1. Age-dependent M
+
+
+
+
